d6b47d18c75ab5924d27c02b9fd8dd27d0ea9808
Oleh Parshyn <ooparshyn@gmail.com>
2021-08-25 14:04:55 +0300

    message as object

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const id = "sfsfsfsfergerkmgmg";
-chrome.runtime.sendMessage(id, "I am message from web page",
+
+chrome.runtime.sendMessage({message: "I am message from web page"},
   function(response) {
    console.log(`response is ${response}`);
   });

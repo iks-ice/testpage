ef98efaec68a698d0d52af3c8ff11b79714b513b
Oleh Parshyn <ooparshyn@gmail.com>
2021-08-25 16:59:34 +0300

    receive response

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ function initConvertVideoProcess() {
   const extensionId = "picpenoodfjockgobmppganpfnpfooio";
   const port = chrome.runtime.connect(extensionId, {name: "Подтвердите отправку видео для конвертации"});
   port.postMessage({message: "Подтвердите отправку видео для конвертации"});
+  port.onMessage.addListener((m) => {
+    console.log(m);
+  })
 }

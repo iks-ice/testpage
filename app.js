ef32df839a420a0328d31ff922b0a28a52c7b307
Oleh Parshyn <olehparshin87@gmail.com>
2021-09-16 12:35:22 +0300

    refactor

--- app.js
+++ app.js
@@ -6,39 +6,31 @@ var constraints = window.constraints = {
 };
 var errorElement = document.querySelector('#errorMsg');
 
-navigator.mediaDevices.getUserMedia(constraints)
-  .then((stream) => {
-    var videoTracks = stream.getVideoTracks();
-    stream.onremovetrack = function() {
-      console.log('Stream ended');
-    };
-    window.stream = stream; // make variable available to browser console
+const getMedia = async (constraints) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
     video.srcObject = stream;
     video.play();
-    initMediaRecorder(stream);
-})
-.catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error)
+  }
+}
+getMedia(constraints);
 
 
 function initMediaRecorder(stream) {
-  console.log("inited");
-  const recordedChunks = [];
+  let recordedChunks = [];
 
   const options = { mimeType: "video/webm; codecs=vp9" };
   const mediaRecorder = new MediaRecorder(stream, options);
 
   mediaRecorder.ondataavailable = handleDataAvailable;
  
-
   function handleDataAvailable(event) {
     console.log("data-available");
     if (event.data.size > 0) {
       recordedChunks.push(event.data);
-      console.log(recordedChunks);
-      //download();
-    } else {
-      // ...
-    }
+    } 
   }
 
 
@@ -47,43 +39,42 @@ function initMediaRecorder(stream) {
   document.querySelector("#stop").addEventListener("click", stopRecord);
 
   function startRecord() {
+    recordedChunks = [];
+    if (mediaRecorder.state === "recording") {
+      return;
+    }
     console.log("recording started");
     mediaRecorder.start();
   }
 
   function stopRecord() {
-    console.log("recording stoped");
+    if (mediaRecorder.state === "inactive") {
+      return;
+    }
     mediaRecorder.stop();
   }
 
   async function initConvertVideoProcess(recordedChunks, options={}) {
-    console.log(recordedChunks);
-    console.log(new Blob(recordedChunks, {
-      type: "video/webm"
-    }));
+
     const extensionId = "picpenoodfjockgobmppganpfnpfooio";
-    const blob = new Blob(recordedChunks, {
+    const videoBlob = new Blob(recordedChunks, {
       type: "video/webm"
     });
-    const blobAsText = await blob.text();
-    console.log(chrome);
     chrome.runtime.sendMessage(extensionId, {
-      message: "Вы отправляете видео на конвертацию",
-      video: "blobAsText",
-      options
+      video: videoBlob,
     });
   }
-  function download() {
-    var blob = new Blob(recordedChunks, {
-      type: "video/webm"
-    });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    a.href = url;
-    a.download = "test.webm";
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }
+  // function download() {
+  //   var blob = new Blob(recordedChunks, {
+  //     type: "video/webm"
+  //   });
+  //   var url = URL.createObjectURL(blob);
+  //   var a = document.createElement("a");
+  //   document.body.appendChild(a);
+  //   a.style = "display: none";
+  //   a.href = url;
+  //   a.download = "test.webm";
+  //   a.click();
+  //   window.URL.revokeObjectURL(url);
+  // }
 }
